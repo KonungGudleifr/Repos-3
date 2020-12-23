e002807d6fe6812afad1996dfe21e3784dac8f53
KonungGudleifr <72839083+KonungGudleifr@users.noreply.github.com>
2020-12-23 19:17:26 +0200

    Add files via upload

---
+++ 2.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChernogorApp2._1
+{
+    class Program //Средний уровень
+    {
+        static double s = 0.05d, p = 0.10d; //скидка
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите стоимость покупки: ");
+                double salary = double.Parse(Console.ReadLine());
+                if (salary > 1000 & salary < 2001)
+                {
+                    salary -= salary * s;
+                }
+                else if (salary > 2000 & salary < 5001)
+                {
+                    salary -= salary * p;
+                }
+                else
+                {
+                    salary = salary;
+                }
+                Console.WriteLine($"Стоимость покупки со скидкой в гривнах составляет: {salary}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
